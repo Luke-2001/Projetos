dbf02a45d180ef55c24a16072bfb1605400df92f
Lucas <83551053+Luke-2001@users.noreply.github.com>
2025-09-13 17:02:04 -0300

    variaveis

---
+++ N1_NC4/n1_nc4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.69;
+    double peso = 85.5;
+    char letra = 'A';
+    char nome[20] = "Lucas";
+}
